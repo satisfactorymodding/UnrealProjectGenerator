3c711f04b20660929ea8a12795cb2ec53f44563b
mircearoata <mircearoatapalade@yahoo.ro>
2019-11-26 21:12:00 +0200

    Prepare for branching

--- updateHeaders.py
+++ updateHeaders.py
@@ -4,24 +4,8 @@ import shutil
 
 path = os.path.dirname(os.path.abspath(__file__))
 
-def getVersion(versionPath):
-    with open(f'{path}\\Headers\\{versionPath}\\currentVersion.txt', 'r') as f:
-        return int(f.read())
-
-print("Is new version experimental? (y/n): ")
-is_experimental = (input() == 'y')
-
-newHeadersPath = "Experimental" if is_experimental else "EarlyAccess"
-
-if is_experimental:
-    if getVersion("EarlyAccess") >= getVersion("Experimental"):
-        oldHeadersPath = "EarlyAccess"
-    else:
-        oldHeadersPath = "Experimental"
-else:
-    oldHeadersPath = "EarlyAccess"
-
-oldVersion = getVersion(oldHeadersPath)
+with open(f'{path}\\Headers\\currentVersion.txt', 'r') as f:
+    oldVersion = int(f.read())
 
 print(f"Detected old version: {oldVersion} ({oldHeadersPath})")
 print("Update version: ")
@@ -29,15 +13,15 @@ newVersion = input()
 print("Updated headers path: ")
 cssHeadersPath = input()
 
-subprocess.call([f"{path}\\FixHeaders\\FixHeaders\\bin\\Debug\\FixHeaders.exe", f"{path}\\Headers\\{oldHeadersPath}\\OriginalHeaders", f"{path}\\Headers\\{oldHeadersPath}\\ModifiedHeaders", cssHeadersPath, f"{path}\\Headers\\{newHeadersPath}\\ModifiedHeaders"])
+subprocess.call([f"{path}\\FixHeaders\\FixHeaders\\bin\\Debug\\FixHeaders.exe", f"{path}\\Headers\\OriginalHeaders", f"{path}\\Headers\\ModifiedHeaders", cssHeadersPath, f"{path}\\Headers\\ModifiedHeaders"])
 
-subprocess.call([f"{path}\\ImplementHeaders\\ImplementHeaders\\bin\\Debug\\ImplementHeaders.exe", f"{path}\\Headers\\{newHeadersPath}\\ModifiedHeaders", f"{path}\\Headers\\{newHeadersPath}\\ModifiedImplementations"])
+subprocess.call([f"{path}\\ImplementHeaders\\ImplementHeaders\\bin\\Debug\\ImplementHeaders.exe", f"{path}\\Headers\\ModifiedHeaders", f"{path}\\Headers\\ModifiedImplementations"])
 
-shutil.rmtree(f"{path}\\Headers\\{newHeadersPath}\\OriginalHeaders", ignore_errors=True)
+shutil.rmtree(f"{path}\\Headers\\OriginalHeaders", ignore_errors=True)
 
-shutil.copytree(cssHeadersPath, f"{path}\\Headers\\{newHeadersPath}\\OriginalHeaders")
+shutil.copytree(cssHeadersPath, f"{path}\\Headers\\OriginalHeaders")
 
-with open(f"{path}\\Headers\\{newHeadersPath}\\currentVersion.txt", 'w') as f:
+with open(f"{path}\\Headers\\currentVersion.txt", 'w') as f:
     f.write(newVersion)
 
 input()
