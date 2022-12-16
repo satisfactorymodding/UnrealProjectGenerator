c85761cc0b7a31377e9ab04ee33a194b57de0317
mircearoata <mircearoatapalade@gmail.com>
2022-12-16 18:58:39 +0100

    Add AFGBuildable AbstractInstance preview functions

--- GenerationScripts/ImplementHeaders/ImplementHeaders/Program.cs
+++ GenerationScripts/ImplementHeaders/ImplementHeaders/Program.cs
@@ -786,6 +786,74 @@ FCustomVersionRegistration GRegisterFactoryGameCustomVersion{ FFactoryGameCustom
 @"  return *Cast<UFGServerManager>(GetOuter());" },
             { "UFGListView::RebuildListWidget",
 @"  return ConstructListView<SFGListView>();" },
+            { "AFGBuildable::RemoveInstances_Native",
+@"  if( this && GetWorld() )
+    {
+        AAbstractInstanceManager* Manager = AAbstractInstanceManager::GetInstanceManager( GetWorld() );
+        for (FInstanceHandle* InstanceHandle : mInstanceHandles )
+        {
+            if( InstanceHandle->IsInstanced() )
+            {
+                Manager->RemoveInstance( InstanceHandle );
+            }
+        }
+        mInstanceHandles.Empty();
+    }"
+            },
+            { "AFGBuildable::SetupInstances_Native",
+@"  if( this && GetWorld() && IsValid( mInstanceDataCDO ) )
+    {
+        for (FInstanceData InstanceData : mInstanceDataCDO->GetInstanceData() )
+        {
+            if( IsValid( InstanceData.StaticMesh ) && !InstanceData.OverridenMaterials.Contains( nullptr ) )
+            {
+                FInstanceHandle* Handle;
+                AAbstractInstanceManager::SetInstanceFromDataStatic( this, FTransform(), InstanceData, Handle, bInitializeHidden );
+                mInstanceHandles.Add( Handle );
+            }
+        }
+    }"
+            },
+            { "AFGBuildable::GetActorLightweightInstanceData_Implementation",
+@"  if( IsValid( mInstanceDataCDO ) )
+    {
+        return mInstanceDataCDO->GetInstanceData();
+    }
+    return TArray<FInstanceData>();"
+            },
+            { "AFGBuildable::OnConstruction",
+@"  Super::OnConstruction(transform);
+
+#if WITH_EDITOR
+    RemoveInstances();
+    RemoveInstances_Native();
+    CallSetupInstances();
+#endif"
+            },
+            { "AFGBuildable::PostEditChangeProperty",
+@"  Super::PostEditChangeProperty(PropertyChangedEvent);
+    if( PropertyChangedEvent.GetPropertyName() == GET_MEMBER_NAME_CHECKED( AFGBuildable, mInstanceData) )
+    {
+        mInstanceDataCDO = mInstanceData;
+    }
+    
+    if( PropertyChangedEvent.GetPropertyName() == GET_MEMBER_NAME_CHECKED( AFGBuildable, mBuildableSparseDataEditorObject) )
+    {
+        mBuildableSparseDataCDO = mBuildableSparseDataEditorObject;
+    }"
+            },
+            { "AFGBuildable::PostEditChangeChainProperty",
+@"  Super::PostEditChangeChainProperty(PropertyChangedEvent);
+    if( PropertyChangedEvent.GetPropertyName() == GET_MEMBER_NAME_CHECKED( AFGBuildable, mInstanceData) )
+    {
+        mInstanceDataCDO = mInstanceData;
+    }
+    
+    if( PropertyChangedEvent.GetPropertyName() == GET_MEMBER_NAME_CHECKED( AFGBuildable, mBuildableSparseDataEditorObject) )
+    {
+        mBuildableSparseDataCDO = mBuildableSparseDataEditorObject;
+    }"
+            },
         };
 
         private static readonly Dictionary<string, string> CustomSuper = new Dictionary<string, string>()
