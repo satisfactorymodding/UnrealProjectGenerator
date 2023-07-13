fa355d0e72fb7dc27bb481cd7f765315e544376b
Mircea Roata <mircearoatapalade@gmail.com>
2023-07-13 17:26:27 +0300

    Add AFGGameMode option strings

---
+++ CustomImplementations/FGGameMode.cpp
@@ -0,0 +1,5 @@
+const TCHAR* AFGGameMode::StartLocationOption = TEXT("startloc");
+const TCHAR* AFGGameMode::LoadGameOption = TEXT("loadgame");
+const TCHAR* AFGGameMode::SkipOnboarding = TEXT("skiponboarding");
+const TCHAR* AFGGameMode::AdvancedGameSettingsOption = TEXT("advancedGameSettings");
+const TCHAR* AFGGameMode::EnableAdvancedGameSettingsOption = TEXT("enableAdvancedGameSettings");
