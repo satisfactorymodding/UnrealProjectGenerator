dfa82771d6ebedf62815d6d2abb4002b0019e0e1
Angel Pons <22937129+Th3Fanbus@users.noreply.github.com>
2024-04-06 22:53:57 +0000

    fix: Make colored instance mesh proxies visible in PIE
    The code already accounted for two editor use-cases, but was missing the
    PIE use-case. Replace the checks with `IsEditorWorld` as it's equivalent
    to having three discrete checks.

--- CustomImplementations/FGColoredInstanceMeshProxy.cpp
+++ CustomImplementations/FGColoredInstanceMeshProxy.cpp
@@ -1,6 +1,6 @@
 bool UFGColoredInstanceMeshProxy::ShouldCreateRenderState() const {
-#if WITH_EDITOR // Ensure visibility in blueprint & editor.
-    if (GetWorld()->WorldType == EWorldType::Editor || GetWorld()->WorldType == EWorldType::EditorPreview) {
+#if WITH_EDITOR // Ensure visibility in blueprint, editor & PIE
+    if (GetWorld()->IsEditorWorld()) {
         return true;
     }
 #endif
